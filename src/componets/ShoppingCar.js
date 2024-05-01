0ab1b8a2116d95301bd22bd22bf96aae296efe3a
isaias_pruebas <isgb_codigo@hotmail.com>
2024-05-01 16:58:38 -0600

    debug

---
+++ src/componets/ShoppingCar.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const ShoppingCar = () => {
+  return (
+    <div>
+        <h2>Carrito de compras</h2>
+        <h3>Productos</h3>
+        <article className='box'></article>
+        <h3>Carrito</h3>
+        <article className='box'></article>
+    </div>
+  )
+}
+
+export default ShoppingCar;
