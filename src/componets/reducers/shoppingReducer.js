0f2c1e8cfad2da89c0b939f799ab506cea2a8f76
isaias_pruebas <isgb_codigo@hotmail.com>
2024-05-01 18:47:24 -0600

    debug

--- src/componets/reducers/shoppingReducer.js
+++ src/componets/reducers/shoppingReducer.js
@@ -15,7 +15,18 @@ export const shoppingInitialState = {
 export const shoppingReducer = (state, action) => {
     switch (action.type) {
         case TYPES.ADD_TO_CART:{
+            let newItem = state.products.find((item) => {
+                item.id === action.payload
+            })
+            // console.log(newItem);
 
+            return{
+                ...state,
+                cart: [
+                    ...state.cart,
+                    newItem
+                ]
+            }
         }
         case TYPES.REMOVE_ONE_FROM_CART:{
 
