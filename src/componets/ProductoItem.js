9de65bc98c9ac30ae0542619ea28205c15db0934
isaias_pruebas <isgb_codigo@hotmail.com>
2024-05-01 18:16:42 -0600

    debug

--- src/componets/ProductoItem.js
+++ src/componets/ProductoItem.js
@@ -1,7 +1,13 @@
 import React from 'react'
 
 export const ProductoItem = ({data, addToCart}) => {
+    const {id,price,name} = data;
+    
   return (
-    <div>ProductoItem</div>
+    <div>
+        <h4>{name}</h4>
+        <h5>{price}</h5>
+        <button onClick={() => addToCart(id) }>Agregar</button>
+    </div>
   )
 }
