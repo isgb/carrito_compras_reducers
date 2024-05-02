c1fc2b44e0293acf0604a18138c0f8015e57dd2c
isaias_pruebas <isgb_codigo@hotmail.com>
2024-05-01 19:45:28 -0600

    debug

--- src/componets/CardItem.js
+++ src/componets/CardItem.js
@@ -4,7 +4,7 @@ const CardItem = ({data, deleteFromCart}) => {
     const {id,name, price} = data;
   return (
     <div>
-        <h1>{id}</h1>
+        <h1>{name}</h1>
         <h5>${price}.00</h5>
         <button>Eliminar</button>
     </div>
